0589d25105419b24b9ec0fc5fce970612fe85e62
Artuz Cezar <157129485+artuzcezar@users.noreply.github.com>
2024-04-30 14:38:06 +0800

    basic.py

--- basic.py
+++ basic.py
@@ -1,15 +1,18 @@
-import math
+
 def savings(employee_gross_pay, employee_tax_rate, employee_expenses):
+    import math
     net_pay_after_taxes = math.floor((1 - employee_tax_rate) * employee_gross_pay)
     remaining_funds = net_pay_after_taxes - employee_expenses
     return remaining_funds
 
-import math
+
 def material_waste(available_material, material_unit, jobs_count, consumption_per_job):
+    import math
     total_waste = available_material - (jobs_count * consumption_per_job)
     return str(total_waste) + material_unit
 
-import math
+
 def interest(starting_principal, interest_rate, investment_periods):
+    import math
     accrued_interest = math.floor((starting_principal * interest_rate * investment_periods) + starting_principal)
     return accrued_interest
